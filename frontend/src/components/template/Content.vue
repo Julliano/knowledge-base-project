a348cdef5e77bb8a8934d8f827a9cb11140ed706
Julliano <julliano.osorio@digitro.com.br>
2019-08-07 13:51:12 -0300

    frontend start to evolve, created grid layout areas (header, menu, content and footer)

---
+++ frontend/src/components/template/Content.vue
@@ -0,0 +1,17 @@
+<style>
+    .content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
+
+<template>
+    <div class="content"></div>
+</template>
+
+<script>
+export default {
+    name: 'Content'
+}
+</script>
---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
+
+<template>
+    <footer class="footer">
+        <span>Copyright <strong>Julliano</strong> © 2014-2019 </span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
